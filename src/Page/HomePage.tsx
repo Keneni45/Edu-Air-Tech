a52b1f77adf0fb8dbe253f249408bc133f0616cb
keneni45 <kenotoneaga@gmail.com>
2023-05-02 13:31:46 -0700

    Add List Item On Footer

--- src/Page/HomePage.tsx
+++ src/Page/HomePage.tsx
@@ -1,32 +1,52 @@
+//import TwitterIcon from "@mui/icons-material/Twitter";
+//import InterpreterModeIcon from "@mui/icons-material/InterpreterMode";
+import { useState } from "react";
 import styles from "./home.module.css";
-import ArrowForwardSharpIcon from "@mui/icons-material/ArrowForwardSharp";
+
 export default function HomePage() {
+  const [isHover, setIsHover] = useState(true);
   return (
     <div className={styles.home}>
       <div className={styles.homeNavbar}>
         <ul className={styles.ulHome}>
-          <li>Home</li>
+          {isHover ? <li>Home</li> : <hr />}
           <li>About</li>
           <li>Resource</li>
           <li>Contact</li>
-          <button className={styles.getStartedButton}>
-            Go To
-            <ArrowForwardSharpIcon style={{ color: "black" }} />
-          </button>
+          <button className={styles.getStartedButton}>Go To</button>
           <button className={styles.exerciseNowButton}>Exercise Now</button>
         </ul>
-      </div>
+      </div>{" "}
       <div className={styles.homeBody}>
         <div className={styles.leftHomeBody}>
           {" "}
           <h1>It's Now To Win Your Exams</h1>
           <h3>The First Ethiopia Exam Archive </h3>
-          <button className={styles.exerciseNowButton}>
-            Get Started
-            <ArrowForwardSharpIcon style={{ color: "black" }} />
-          </button>
+          <button className={styles.exerciseNowButton}>Get Started</button>
         </div>
         <div className={styles.rightHomeBody}>image</div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.leftFooter}>
+          <h2>Contact</h2>
+          <div>
+            <ul className={styles.listFooter}>
+              <li>Follow us :</li>
+              <li>Facebook</li>
+              <li>Linkedin</li>
+              <li>twittwer</li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.rightFooter}>
+          <div>
+            <ul>
+              <li>Community</li>
+              <li>Community</li>
+              <li>Community</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
