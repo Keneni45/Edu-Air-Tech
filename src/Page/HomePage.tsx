c0766a5a97588c486f1b41f0bef7bd57af25019c
keneni45 <kenotoneaga@gmail.com>
2023-05-04 05:00:25 -0700

    CSS ADD

--- src/Page/HomePage.tsx
+++ src/Page/HomePage.tsx
@@ -1,18 +1,48 @@
-//import TwitterIcon from "@mui/icons-material/Twitter";
-//import InterpreterModeIcon from "@mui/icons-material/InterpreterMode";
 import { useState } from "react";
 import styles from "./home.module.css";
+//import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 export default function HomePage() {
   const [isHover, setIsHover] = useState(true);
+
+  function handleClick(e: any) {
+    setIsHover(e.target.value);
+  }
   return (
     <div className={styles.home}>
       <div className={styles.homeNavbar}>
         <ul className={styles.ulHome}>
-          {isHover ? <li>Home</li> : <hr />}
-          <li>About</li>
-          <li>Resource</li>
-          <li>Contact</li>
+          {isHover ? (
+            <li>Home</li>
+          ) : (
+            <li onClick={handleClick}>
+              Home <hr />
+            </li>
+          )}
+          {isHover ? (
+            <li onClick={handleClick}>About</li>
+          ) : (
+            <li>
+              About <hr />
+            </li>
+          )}
+
+          {isHover ? (
+            <li>Resource</li>
+          ) : (
+            <li>
+              Resource <hr />
+            </li>
+          )}
+
+          {isHover ? (
+            <li>Contact</li>
+          ) : (
+            <li>
+              Contact <hr />
+            </li>
+          )}
+
           <button className={styles.getStartedButton}>Go To</button>
           <button className={styles.exerciseNowButton}>Exercise Now</button>
         </ul>
@@ -25,6 +55,30 @@ export default function HomePage() {
           <button className={styles.exerciseNowButton}>Get Started</button>
         </div>
         <div className={styles.rightHomeBody}>image</div>
+      </div>
+      <div>
+        {" "}
+        <h2>Resource</h2>
+      </div>
+      <div className={styles.resource}>
+        <div className={styles.leftResource}>
+          Grade 9 <hr />
+        </div>
+        <div className={styles.rightResource}>image</div>
+      </div>
+      <div>
+        About <hr />
+      </div>
+      <div className={styles.about}>
+        <div className={styles.leftAbout}>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit,
+            fugiat quaerat. Rem autem error eaque sunt ex, consequatur illum
+            adipisci magni, tenetur suscipit magnam odit qui sint fugiat libero
+            dolores.
+          </p>
+        </div>
+        <div></div>
       </div>
       <div className={styles.footer}>
         <div className={styles.leftFooter}>
