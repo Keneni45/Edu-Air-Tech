b3db81ca21c86ff9d31a83fb11d0d3d48f999d03
keneni45 <kenotoneaga@gmail.com>
2023-05-13 05:33:02 -0700

    Add User Info List Item

--- src/Page/ChallengeBoard/Challenge.tsx
+++ src/Page/ChallengeBoard/Challenge.tsx
@@ -1,4 +1,5 @@
 import styles from "./challenge.module.css";
+import SearchSharpIcon from "@mui/icons-material/SearchSharp";
 
 export default function Challenge() {
   return (
@@ -7,14 +8,12 @@ export default function Challenge() {
         <div className={styles.board}>
           <div className={styles.leftBoard}>
             <div className={styles.smartTable}>
-              {" "}
               <h1>
                 SMART <br /> CHALLENGE
-              </h1>{" "}
+              </h1>
             </div>
             <div className={styles.leaderText}>
-              {" "}
-              <h3>LeaderBoard</h3>{" "}
+              <h3>LeaderBoard</h3>
             </div>
             <div className={styles.userInfo}>profile</div>
             <div className={styles.performanceText}>
@@ -29,8 +28,44 @@ export default function Challenge() {
             </h1>
             <div className={styles.challengeTable}>
               <div className={styles.tableHeader}>
-                <div className={styles.leftHeader}>right</div>
-                <div className={styles.rightHeader}>left</div>
+                <div className={styles.leftHeader}>
+                  <ul className={styles.leftList}>
+                    <li>
+                      <h3>RANK</h3>
+                    </li>
+                    <li>
+                      <h3>NAME</h3>
+                    </li>
+                    <li>
+                      <h3>SCORE</h3>
+                    </li>
+                    <li>
+                      <h3>FRIENDS SCORE</h3>
+                    </li>
+                  </ul>
+                  <hr
+                    style={{
+                      marginLeft: "38px",
+                      marginTop: "10px",
+                      backgroundColor: "blue",
+                    }}
+                  />
+                </div>
+                <div className={styles.rightHeader}>
+                  Search <SearchSharpIcon />
+                </div>
+              </div>
+              <div className={styles.userContainer}>
+                <div className={styles.userDetailInfo}>
+                  <div className={styles.userDeatilInfoList}>
+                    <div>right</div>
+                    <div>center</div>
+                    <div>Name</div>
+                    <div>left</div>
+                  </div>
+                </div>
+                <div>Challenge</div>
+                <div>View Profile</div>
               </div>
             </div>
           </div>
