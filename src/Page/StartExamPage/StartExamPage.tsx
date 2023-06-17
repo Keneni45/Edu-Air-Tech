f165df1d8744ba6ffb1754ccb52e379c34e6f52c
keneni45 <kenotoneaga@gmail.com>
2023-06-17 06:55:43 -0700

    Fix:code

--- src/Page/StartExamPage/StartExamPage.tsx
+++ src/Page/StartExamPage/StartExamPage.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function StartExamPage() {
+  const location = useLocation();
+  const data = location.state?.data;
   return (
     <div>
       <p>You are gone take chemistry exam </p>
-      <Link to="/exercise">
+      <Link to="/exercise" state={data}>
         <Button variant="contained">Start Exam</Button>
       </Link>
     </div>
